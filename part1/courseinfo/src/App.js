f9e4fc151c8538e5b00b34c92e3945e2ff4c83b9
Admin <albertoanchuste@gmail.com>
2022-06-28 23:41:48 +0200

    Exercise 1.1 solved

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,28 +8,45 @@ const Header = (props) =>{
   )
 };
 
-const App = () => {
-  //const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Content = (props) =>{
+  return(
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+};
 
+const Total = (props) =>{
+  var totalExercises = Number(props.exercises1) + 
+  Number(props.exercises2) + Number(props.exercises3);
+  return(
+    <div>
+      <p>
+      Number of exercises: {totalExercises}
+      </p>
+    </div>
+  )
+};
+
+const App = () => {
   return (
     <div>
       <Header title='Half Stack application development'/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part1='Fundamentals of React'
+                exercises1 = '10'
+                part2 = 'Using props to pass data'
+                exercises2 = '7'
+                part3 = 'State of a component'
+                exercises3 = '14'/>
+      <Total exercises1 = '10' exercises2 = '7' exercises3 = '14'/>
     </div>
   )
 }
